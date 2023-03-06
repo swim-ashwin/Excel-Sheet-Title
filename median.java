f82ae71cb573fadf6501863e896494b274295242
swim-ashwin <125240172+swim-ashwin@users.noreply.github.com>
2023-03-05 21:28:01 -0800

    Implemented Method!

--- median.java
+++ median.java
@@ -5,7 +5,19 @@ public class median{
     }
 
     public int[] merge(int[] arr1, int[] arr2){
-        return null;//implment merge method
+        int aLength = arr1.length;
+        int bLength = arr2.length;
+        int totalLength = aLength + bLength;
+    
+        int[] c = new int[totalLength];
+        for (int i = 0; i < aLength; i++) {
+            c[i] = arr1[i];
+        }
+        for (int i = 0; i < bLength; i++) {
+            c[aLength + i] = arr2[i];
+        }
+    
+        return c;
     }
 
     public int[] sort(int[] mergedArray){
