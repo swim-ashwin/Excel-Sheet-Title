b65194b2c538d6623ba344938a0b644300489d78
swim-ashwin <125240172+swim-ashwin@users.noreply.github.com>
2023-03-05 21:19:44 -0800

    Implement Merge method

---
+++ median.java
@@ -0,0 +1,20 @@
+public class median{
+
+    public static void main(){
+
+    }
+
+    public int[] merge(int[] arr1, int[] arr2){
+        return null;//implment merge method
+    }
+
+    public int[] sort(int[] mergedArray){
+        return null;
+    }
+
+    public int findMedian(int[] rearranged){
+        return 0;
+    }
+    
+
+}
